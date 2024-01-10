1d1aa5e0df1922cac222bff83b19cadf2511b8e2
AntoniaLivia79 <antoniablanchflower@gmail.com>
2024-01-10 20:15:31 +0000

    Refactor code to allow individual lines to be drawn

--- src/VectorPaint.py
+++ src/VectorPaint.py
@@ -13,51 +13,61 @@ def main():
     window = pygame.display.set_mode((600, 600))
     clock = pygame.time.Clock()
 
-    lines = [(0, 0)]
-    draw = False
-    plot = False
-
-    #plot_pos = (0,0)
+    lines = []
+    plot_position = (0, 0)
+    source = (0, 0)
+    destination = (0, 0)
+    drawing_state = False
+    source_state = False
 
     matrix = [tuple([elem * 50 for elem in (x, y)]) for x in range(1, 11) for y in range(1, 11)]
     run = True
 
     while run:
+        # Update the pygame clock
         clock.tick(60)
+
+        # Render the current drawing (function)
+        # Fill screen with black
+        window.fill((0, 0, 0))
+        # Draw the node matrix
+        for node in matrix:
+            pygame.draw.circle(window, Config.itempalette, node, 5)
+        # Draw all stored lines
+        for line in lines:
+            pygame.draw.line(window, Config.entitypalette, line[0], line[1], 5)
+
+        # Detect pygame event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if hasattr(event, 'pos'):
-                plot_pos = event.pos
+                plot_position = event.pos
 
-            if event.type == pygame.MOUSEBUTTONDOWN and not plot and is_close_to_any(event.pos, matrix)[0]:
-                draw = True
-                new_pos = is_close_to_any(event.pos, matrix)[1]
-
-                plot = True
-                if lines == [(0,0)]:
-                    lines = [new_pos]
+            # Add a drawn line to the lines list (function)
+            # Detect if clicked mouse, drawing line is in progress and near to a node
+            if event.type == pygame.MOUSEBUTTONDOWN and is_close_to_any(plot_position, matrix)[0]:
+                if not drawing_state and not source_state:
+                    # A new line is now being drawn, change the drawing state
+                    drawing_state = True
+                    # Store the source coordinate which has just been plotted and change the source state
+                    source = is_close_to_any(plot_position, matrix)[1]
+                    source_state = True
                 else:
-                    lines.append(new_pos)
-            else:
-                if event.type == pygame.MOUSEBUTTONUP and plot:
-                    plot = False
+                    # Drawing a line is now complete, reset the drawing state
+                    drawing_state = False
+                    # Find the node closest to the mouse coordinate
+                    destination = is_close_to_any(plot_position, matrix)[1]
+                    # Store the source and destination coordinates of the drawn line
+                    lines.append([source, destination])
+                    # Reset the source and destination states
+                    source_state = False
 
-        window.fill((0, 0, 0))
-        old_points = (0, 0)
+            # Draw a line if source exists but not drawing is not yet stored in lines list (function)
+            if source_state:
+                pygame.draw.line(window, Config.entitypalette, source, plot_position, 5)
 
-        for node in matrix:
-            pygame.draw.circle(window, Config.itempalette, node, 5)
-
-        for points in lines:
-            if points != old_points and points[0] != 0:
-                if old_points != (0,0):
-                    pygame.draw.line(window, Config.entitypalette, old_points, points, 5)
-                old_points = points
-
-        if draw:
-            pygame.draw.line(window, Config.entitypalette, points, plot_pos, 5)
-
+        # Update the display
         pygame.display.flip()
 
         # Print the list of coordinates
