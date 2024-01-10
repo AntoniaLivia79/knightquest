65ac4abd6abffdc15b9e5d51770a49337f971afd
AntoniaLivia79 <antoniablanchflower@gmail.com>
2024-01-10 21:27:31 +0000

    Add buttons to export json and quit app

--- src/VectorPaint.py
+++ src/VectorPaint.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 import Config as Config
 
 
@@ -9,14 +10,29 @@ def main():
                 return [True, c]
         return [False, (0,0)]
 
+    def export_to_json():
+        json_data = {"entity_image_vectors": [[{'x': int(src[0]/50) - 1, 'y': int(src[1]/50) - 1},
+                      {'x': int(dest[0]/50) - 1, 'y': int(dest[1]/50) - 1}] for src, dest in lines]}
+
+        with open('exported_lines.json', 'w') as json_file:
+            json.dump(json_data, json_file, indent=2)
+        print('Lines exported to exported_lines.json')
+
     pygame.init()
     window = pygame.display.set_mode((600, 600))
     clock = pygame.time.Clock()
 
+    # Define colors
+    black = (0, 0, 0)
+    button_colour = Config.entitypalette
+    button_border_colour = Config.itempalette
+
+    # Define font
+    font = pygame.font.Font(None, 14)
+
     lines = []
     plot_position = (0, 0)
     source = (0, 0)
-
     drawing_state = False
     source_state = False
 
@@ -29,7 +45,17 @@ def main():
 
         # Render the current drawing (function)
         # Fill screen with black
-        window.fill((0, 0, 0))
+        window.fill(black)
+        # Draw export button
+        button_text = font.render("Export to JSON", True, button_border_colour)
+        button_export = pygame.Rect(5, 5, button_text.get_width() + 10, button_text.get_height() + 10)
+        pygame.draw.rect(window, button_colour, button_export)
+        window.blit(button_text, (button_export.x + 5, button_export.y + 5))
+        # Draw quit button
+        button_text = font.render("Quit", True, button_border_colour)
+        button_quit = pygame.Rect(200, 5, button_text.get_width() + 10, button_text.get_height() + 10)
+        pygame.draw.rect(window, button_colour, button_quit)
+        window.blit(button_text, (button_quit.x + 5, button_quit.y + 5))
         # Draw the node matrix
         for node in matrix:
             pygame.draw.circle(window, Config.itempalette, node, 5)
@@ -50,30 +76,38 @@ def main():
 
             # Add a drawn line to the lines list (function)
             # Detect if clicked mouse, drawing line is in progress and near to a node
-            if event.type == pygame.MOUSEBUTTONDOWN and is_close_to_any(plot_position, matrix)[0]:
-                # On left mouse button click, begin drawing line or complete drawing line
-                if event.button == 1:
-                    if not drawing_state and not source_state:
-                        # A new line is now being drawn, change the drawing state
-                        drawing_state = True
-                        # Store the source coordinate which has just been plotted and change the source state
-                        source = is_close_to_any(plot_position, matrix)[1]
-                        source_state = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if button_export.collidepoint(mouse_pos):
+                    export_to_json()
+                else:
+                    if button_quit.collidepoint(mouse_pos):
+                        run = False
                     else:
-                        # Drawing a line is now complete, reset the drawing state
-                        drawing_state = False
-                        # Find the node closest to the mouse coordinate
-                        destination = is_close_to_any(plot_position, matrix)[1]
-                        # Store the source and destination coordinates of the drawn line
-                        lines.append([source, destination])
-                        # Reset the source and destination states
-                        source_state = False
-                # On right mouse button click, clear any drawn lines from the lines list that contain the clicked node
-                if event.button == 3:
-                    source = is_close_to_any(plot_position, matrix)[1]
-                    for line in lines:
-                        if line[0] == source or line[1] == source:
-                            lines.remove(line)
+                        if is_close_to_any(plot_position, matrix)[0]:
+                            # On left mouse button click, begin drawing line or complete drawing line
+                            if event.button == 1:
+                                if not drawing_state and not source_state:
+                                    # A new line is now being drawn, change the drawing state
+                                    drawing_state = True
+                                    # Store the source coordinate which has just been plotted and change the source state
+                                    source = is_close_to_any(plot_position, matrix)[1]
+                                    source_state = True
+                                else:
+                                    # Drawing a line is now complete, reset the drawing state
+                                    drawing_state = False
+                                    # Find the node closest to the mouse coordinate
+                                    destination = is_close_to_any(plot_position, matrix)[1]
+                                    # Store the source and destination coordinates of the drawn line
+                                    lines.append([source, destination])
+                                    # Reset the source and destination states
+                                    source_state = False
+                            # On right mouse button click, clear any lines from the lines list containing the clicked node
+                            if event.button == 3:
+                                source = is_close_to_any(plot_position, matrix)[1]
+                                for line in lines:
+                                    if line[0] == source or line[1] == source:
+                                        lines.remove(line)
 
             # Draw a line if source exists but not drawing is not yet stored in lines list (function)
             if source_state:
